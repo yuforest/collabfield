767b761595ab359d264c65c96e17143e8aa6c3bd
大森優太 <yuforest@oomoriyuuta-no-MacBook-Pro.local>
2018-09-22 21:20:56 +0700

    add sign up features specs

--- spec/features/user/login_spec.rb
+++ spec/features/user/login_spec.rb
@@ -4,13 +4,15 @@ RSpec.feature "Login", :type => :feature do
   let(:user) { create(:user) }
 
   scenario 'user navigates to the login page and succesfully logs in', js: true do
-    user
+    # user
+    # visit root_path
+    # find('nav a', text: 'Login').click
+    # fill_in 'user[email]', with: user.email
+    # fill_in 'user[password]', with: user.password
+    # find('.login-button').click
+    sign_in user
     visit root_path
-    find('nav a', text: 'Login').click
-    fill_in 'user[email]', with: user.email
-    fill_in 'user[password]', with: user.password
-    find('.login-button').click
-    expect(page).to have_selector('.login-button')
+    expect(page).to have_selector('#user-settings')
   end
 
 end
