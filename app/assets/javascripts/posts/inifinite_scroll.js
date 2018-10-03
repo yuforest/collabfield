3cff015244cd0206ea35668d3892234522e0d714
大森優太 <>
2018-10-03 09:54:43 +0900

    Add posts from all branches in the home page - Modify the _main_content.html.erb file - Define instance variables inside the PagesController’s index action - Modify the no_posts_partial_path helper method to be more reusable - Add CSS to style the home page

--- app/assets/javascripts/posts/inifinite_scroll.js
+++ app/assets/javascripts/posts/inifinite_scroll.js
@@ -1,5 +1,5 @@
 $(document).on('turbolinks:load', function(){
-  var isLoading = falsel
+  var isLoading = false
   if ($('.infinite-scroll', this).size() > 0) {
     $(window).on('scroll', function(){
       var more_posts_url = $('.pagination a.next_page').attr('href');
