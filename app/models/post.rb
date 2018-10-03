3702b91ddb3b4362134557865866151a13129cb7
大森優太 <>
2018-10-03 14:58:30 +0900

    Add validations to the Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -15,5 +15,8 @@ class Post < ApplicationRecord
     where("title ILIKE lower(?) OR content ILIKE lower(?)", "%#{search}%", "%#{search}%")
   end
 
+  validates :title, presence: true, length: { minimum: 5, maximum: 255 }
+  validates :content, presence: true, length: { minimum: 20, maximum: 1000 }
+  validates :category_id, presence: true
 
 end
